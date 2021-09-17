62035b06880c19fa88a1de7fd715491e4b020667
Jerio <houjie20@gmail.com>
2021-09-17 22:51:46 +0800

    寻找两个正序数组的中位数

--- src/FindMedianSortedArrays.java
+++ src/FindMedianSortedArrays.java
@@ -1,4 +1,9 @@
-public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+/**
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ */
+public class FindMedianSortedArrays{
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int i= 0,j = 0;
 		int n1 = nums1.length,n2 = nums2.length;
 		int total = n1 + n2;
@@ -31,3 +36,6 @@ public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
 			return(double)merge[cursor-1];
 		}
 	}
+}
+
+
