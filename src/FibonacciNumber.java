4bc7f600d13dd32de1baf585d635493f5041fd4d
JerioHou <houjie20@gmail.com>
2021-10-28 15:28:45 +0800

    509. 斐波那契数

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -11,6 +11,21 @@
  */
 public class FibonacciNumber {
 
-
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return  1;
+        }
+        int p,q = 0;
+        int r = 1;
+        for (int i = 2; i <= n; i++) {
+             p = q;
+             q = r;
+             r = p + q;
+        }
+        return r;
+    }
 
 }
